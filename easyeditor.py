21a7764f6bfbb8928db7b13b5a9b929904a61c71
DanielRCaponius <146859076+DanielRCaponius@users.noreply.github.com>
2023-10-03 19:01:30 +0300

    Update easyeditor.py

--- easyeditor.py
+++ easyeditor.py
@@ -1,4 +1,4 @@
-#Импорт модулей
+#Modules import
 import os
 import io
 
@@ -8,14 +8,10 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QLabel, QFileDialog, QMainWindow, QAction, QSizePolicy, QMessageBox, QScrollArea, QSlider
 from PyQt5.QtGui import QPixmap, QImage
 
-#Первая итерация импорта модулей
-#from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QGroupBox, QTextEdit, QLineEdit, QListWidget, QListWidgetItem, QInputDialog, QMessageBox, QGroupBox , QFileDialog, QSlider, QScrollArea, QMainWindow, QAction, QSizePolicy
-
-#Список со всеми совершенными действиями.
+#All actions list (a try to make undo fucntion)
 #actions = []
 
-# Класс "Действие", создаётся действие, его шорткат, заметка о нём,
-# к какому разделу привязывается, функция, выполняемая при нажатии.
+# Class actions , it's name, shortcut, tip for it, in which bar located, function, enability
 class action():
     def __init__(self,name,shortcut,tip,bar,function,enable):
         self.name = QAction(name)
@@ -25,70 +21,67 @@ class action():
         self.function = self.name.triggered.connect(function)
         self.enable = self.name.setEnabled(enable)
 
-# Класс изображения (вторая итерация).
+# Image class
 class classimg():
-    # Конструктор
+    # Class constructor
     def __init__(self,image,img,qimg,pixmap):
         self.image = image
         self.img = img
         self.qimg = qimg
         self.pixmap = pixmap
 
-    # Открытие изображения
+    # Image opening
     def open_image(self):
-        #Выбор изображения через проводник
+        # Choose image through file explorer
         self.image = QFileDialog.getOpenFileName(None,'Open Image',os.path.expanduser('~\OneDrive'),"png files (*.png);;jpg files (*.jpg)")
-        #Открытие доступа к кнопкам:
+        # Making all buttons available after opening image
         
         for act in available:
             act.name.setEnabled(True)
         
-        #Создания пиксмапа, PILовского изображения и его загрузка, [0] для того, чтобы это был не кортеж.
+        #Creating pixmap, PIL image and loading it.
         self.pixmap = QPixmap(self.image[0])
         self.img = Image.open(self.image[0])
         self.img.load()
         
-        #Добавление в список совершенных действий, отображение последнего элемента списка.
+        #Part of undo function, list of all pixmaps
         #actions = [self.pixmap]
 
-        #Устанока пиксмапа и подгонка окна под его размер
+        #Setting pixmap with correct size
         Label.setPixmap(self.pixmap)
         Label.resize(Label.pixmap().size())
         
-        #Показ ползунков для скролла изображения
+        #Setting scrolls visible
         vscroll.setVisible(True)
-    # Сохранение изображения
+    # Saving image
     def save_image(self):
-        #Открытие проводника для сохранения изображения.
+        #Saving image through file explorer
         filepath, _ = QFileDialog.getSaveFileName(None,'Save Image',os.path.expanduser('~\OneDrive'),"png files (*.png);;jpg files (*.jpg)")
         
-        #Сохранение по заданному пути в выбранной папке и прочем.
+        #Saving image by filepath
         self.img.save(filepath)
 
-    # Конвертация PILовского изображения в пиксмап
+    # Conversion of PILimage to QPixmap
     def pil2pix(self,function):
-        #PILовское изображение приравнивается к редактированному функцией (указывается в function)
+        #PILimage equalts to redacted one by function
         self.img = function
         
-        #Сохранение PILовского изображения в битах для загрузки в QImage
+        #Saving PILimage as bytes for conversion in QImage
         bytesimg = io.BytesIO()
         self.img.save(bytesimg, format='PNG')
 
-        #Создание QImage, в которое загружается сохраненное в битах PILовское изобр.
+        #Creating QImage, then we load bytes in it.
         self.qimg = QImage()
         self.qimg.loadFromData(bytesimg.getvalue())
         
-        #Конвертация QImage в пиксмап
+        #Convert QImage to pixmap
         self.pixmap = QPixmap.fromImage(self.qimg)
         
-        #Установка пиксмапа.
+        #Setting pixmap
         Label.setPixmap(self.pixmap)
         Label.resize(Label.pixmap().size())
 
-
-        #Добавление пиксмапа в список совершенных действий.
-
-    #Попытка сделать UNDO
+    #Try to make undo
     '''def undo(self):
         if len(actions) == 1:
             None
@@ -96,79 +89,78 @@ class classimg():
             actions.pop()
             Label.setPixmap(actions[-1])'''
         
-    # Отражение по вертикали
+    # Mirror by vertical
     def mirror_y(self):
         imgobj.pil2pix(self.img.transpose(Image.FLIP_TOP_BOTTOM))
 
-    # Отражение по горизонтали
+    # Mirror by horizontal
     def mirror_x(self):
         imgobj.pil2pix(self.img.transpose(Image.FLIP_LEFT_RIGHT))
 
-    # Поворот вправо по заданному в ползунке углу
+    # Rotate image by slider (It's works cursed ._.)
     def rotate(self,value):
         RotateSlider.show()
         imgobj.pil2pix(self.img.rotate(value))
 
-    # Поворот обратно на 0 градусов.
+    # Rotate to 0 (doesn't work as expected)
     def rotate_0(self):
         imgobj.pil2pix(self.img.rotate(0))
 
-    # Повоорот вправо/влево на 15 градусов
+    # Rotate right/left by 15 dergrees.
     def rotate_15_r(self):
         imgobj.pil2pix(self.img.rotate(15))
 
     def rotate_15_l(self):
         imgobj.pil2pix(self.img.rotate(-15))
 
-    # Наложение блюра (обычного)
+    # Regular blur
     def blur(self):
         imgobj.pil2pix(self.img.filter(ImageFilter.BLUR))
 
-    # Наложение боксового блюра с заданным радиусом
+    # Box blur with radius on slider (same thing...)
     def box_blur(self,value): 
         BlurSlider.show()
         BlurSlider.setValue(0)
         imgobj.pil2pix(self.img.filter(ImageFilter.BoxBlur(value)))
 
-    # Наложение гауссовского блюра с заданным радиусом
+    # Gauss blur with radius on slider.
     def gauss_blur(self,value):    
         BlurSlider.show()
         BlurSlider.setValue(0)
         imgobj.pil2pix(self.img.filter(ImageFilter.GaussianBlur(value)))
 
-    # Конвертировать изображение в чёрно-белое.
+    # Black and white
     def bw(self):
         imgobj.pil2pix(self.img.convert('L'))
 
-    # Обрезка, не будет реализовано.
 
-# Создание приложения.
+# Creating app
 
 app = QApplication([])
 
-# Создания окна.
+# Creating window
 main_window0 = QMainWindow()
 main_window0.setWindowTitle('Photoshop...')
 main_window0.resize(1280,720)
 
-# Месседж бокс с ошибкой
+# Error message box
 error = QMessageBox()
 error.setWindowTitle('Ошибка')
 error.hide()
 
-# QLabel для изображения.
+# QLabel for image.
 Label = QLabel('Image')
 
-# Прокрутка изображения
+# Scoll image
 vscroll = QScrollArea()
 vscroll.setWidget(Label)
 vscroll.setVisible(False)
 main_window0.setCentralWidget(vscroll)
 vscroll.show()
-# Объект изображения
+# Image object
 imgobj = classimg(None,None,None,None)
 
-#Ползунок для блюра.
+# Blur slider
 BlurSlider = QSlider(Qt.Horizontal)
 
 BlurSlider.setMinimum(0)
@@ -182,7 +174,7 @@ BlurSlider.hide()
 BlurSlider.valueChanged.connect(imgobj.gauss_blur)
 BlurSlider.valueChanged.connect(imgobj.box_blur)
 
-#Ползунок для поворота.
+#Rotate slider
 
 RotateSlider = QSlider(Qt.Horizontal)
 
@@ -196,7 +188,7 @@ RotateSlider.hide()
 
 RotateSlider.valueChanged.connect(imgobj.rotate)
 
-#Создание менюшек для последующего добавления опций
+#Menubars
 menubar = main_window0.menuBar()
 filemenu = menubar.addMenu('File')
 view = menubar.addMenu('View')
@@ -204,33 +196,33 @@ filters = menubar.addMenu('Filters')
 blurmenu = filters.addMenu('Blur')
 colormenu = filters.addMenu('Color')
 
-#Создание действий
-# Выход, открытие файла, сохранение файла как
+# Creating actions
+# Exit applicaton , open file, save file as
 exitapp = action('&Quit','Ctrl+Q','Exit application',filemenu,app.quit,True)
 openfile = action('&Open file','Ctrl+O','Open existing file',filemenu,imgobj.open_image,True)
 savefile = action('&Save file as','Ctrl+S','Save this file',filemenu,imgobj.save_image,False)
 
 #undo = action('Undo','Ctrl+Z','Return back',filemenu,imgobj.undo,False)
 
-#Меню с наложением блюра.
-# Нормальный, гауссовский, боксовый
+# Blursbar
+# Normal, gauss, box
 normal = action('&Normal','Alt+N','Use normal blur',blurmenu,imgobj.blur,False)
 gauss = action('&Gaussian','Alt+G','Use gaussian blur',blurmenu,imgobj.gauss_blur,False)
 box = action('&Box','Alt+B','Use box blur',blurmenu,imgobj.box_blur,False)
 
-#Меню с сменой цвета
-# Чёрно-белый
+# Colorsbar
+# Blackandwhite
 bw = action('Black and white','Shift+B','Use black and white filter',colormenu,imgobj.bw,False)
 
-# Меню с видом изображения
-# Отражение по горизонтали/вертикали, поворот направо/налево/обратно
+# Image view
+# Mirror by h/v, rortate, rotate15r/l, return to 0
 mirrorx = action('Mirror(x)','M','Mirror by x axis',view,imgobj.mirror_x,False)
 mirrory = action('Mirror(y)','Shift+M','Mirror by y axis',view,imgobj.mirror_y,False)
 rotateright = action('Rotate','R','Rotate by value',view,imgobj.rotate,False)
 rotate15r = action('Rotate right (15)','6','Rotate right by 15 degrees',view,imgobj.rotate_15_r,False)
 rotate15l = action('Rotate left (15)','4','Rotate left by 15 degrees',view,imgobj.rotate_15_l,False)
 #return_pos = action('Rotate back','5','Rotate back to 0 degrees',view,imgobj.rotate_0,False)
-
+#All bar parts
 available = [exitapp,openfile,savefile,normal,gauss,box,bw,mirrorx,mirrory,rotateright,rotate15l,rotate15r]
 
 
